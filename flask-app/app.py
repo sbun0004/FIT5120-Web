ddc0d63acb517679a9722d06fd7ee1fc04cb74dd
Sulthan <sbun0004@student.monash.edu>
2023-03-10 13:04:37 +0800

    added flask and venv

---
+++ flask-app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
+
+@app.route('/volunteer')
+def volunteer():
+    return 'Volunteer'
