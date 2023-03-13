1dc09bba0437f7cadb37a031138addc1b7f882b8
Sulthan <sbun0004@student.monash.edu>
2023-03-14 07:56:12 +0800

    added map to the volunteering

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
+GoogleMaps(app)
+
+app.config['GOOGLEMAPS_KEY'] = "8JZ7i18MjFuM35dJHq70n3Hx4"
 
 @app.route('/')
 def home():
@@ -7,7 +13,14 @@ def home():
 
 @app.route('/volunteering')
 def volunteering():
-    return render_template('volunteering.html')
+    markers=[
+   {
+   'lon':143.93109284099341,
+   'lat':-36.96788448566131,
+   'popup':'Victoria'
+    }
+   ]
+    return render_template('volunteering.html',markers=markers)
 
 @app.route('/history')
 def history():
